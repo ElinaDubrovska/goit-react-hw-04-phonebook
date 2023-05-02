99145a9a9ff697720236c81f819cc87da85180a6
ElinaDubrovska <elinadubrovska1801@gmail.com>
2023-05-02 19:45:25 +0300

    add hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,84 +1,70 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Layout } from './Layout/Layout';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 import { nanoid } from 'nanoid';
-import initialContacts from './contacts.json'
+import initialContacts from './contacts.json';
 
-
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-    
-  };
-
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('contacts');
-    if (savedContacts  !== null) {
-      
-      this.setState({ contacts: JSON.parse(savedContacts ) });
-    } else {
-    
-      this.setState({ contacts: initialContacts});
-    }
+const getInitialContacts = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  if (savedContacts !== null) {
+    return JSON.parse(savedContacts);
+  } else {
+    return initialContacts;
   }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = data => {
-    const contact = {
-     id: nanoid,
-     name: data.name,
-     number: data.number,
-   };
-
-   if (this.isDuplicate(contact)) {
-     return alert(`${contact.name} is already in contacts`);
-   }
-   this.setState(prevState => ({
-     contacts: [...prevState.contacts, data]
-   }))
- 
- }
-
- isDuplicate({ name }) {
-   const { contacts } = this.state;
-   const result = contacts.find((item) => item.name === name);
-   return result;
- }
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-changeFilter = e => { 
-  this.setState({ filter: e.currentTarget.value});
 };
 
-  render() {
-    const {filter} = this.state;
+export const App = () => { 
+  const [contacts, setContacts] = useState(getInitialContacts);
+  const [filter, setFilter] = useState('');
 
-    const normalizedFilter = this.state.filter.toLowerCase();
-    const visibleContacts = this.state.contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
 
-    return (
-      <Layout>
-        <h2>Phonebook</h2>
-        <ContactForm onSave={this.addContact} />
-        
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter}/>
-        <ContactList items={visibleContacts}   onDelete={this.deleteContact}/>
-        
-      </Layout>
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = data => {
+    const contact = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
+
+    if (isDuplicate(contact)) {
+      return alert(`${contact.name} is already in contacts`);
+    }
+    setContacts(prevContacts => [...prevContacts, contact]);
+  };
+
+  const isDuplicate = ({ name }) => {
+    return contacts.some(item => item.name === name);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
     );
   };
+
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
+  return (
+    <Layout>
+      <h2>Phonebook</h2>
+      <ContactForm onSave={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ContactList items={visibleContacts} onDelete={deleteContact} />
+    </Layout>
+  );
 }
--- src/components/ContactCard/ContactCard.jsx
+++ src/components/ContactCard/ContactCard.jsx
@@ -1,25 +1,19 @@
-import { Component } from 'react';
+
 import { Button, Container, Name, Number } from './ContactCard.styled';
 
-// import PropTypes from 'prop-types';
 
-export class ContactCard extends Component {
 
-  render() {
- 
-    const {
+export const ContactCard = ( {
+
       item: { id, name, number},
       onDelete,
-    } = this.props;
-
-    return (
+}) =>(
       <Container>
       <Name>
         {name}</Name>:<Number>{number}</Number>
       <Button aria-label="Delete" onClick={() => onDelete(id)}>
              Delete 
             </Button>
-      </Container>
-    );
-  }
-}
+      </Container>)
+
+ 
